99d78953ad77b4a0e0ec6ce57e182abbbbb6b796
Miguel Elias dos Santos <migueleliasweb@gmail.com>
2022-07-28 10:10:07 +1000

    Basic server with a delay in every request.

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	runtime.GOMAXPROCS(1)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Please use \"/loadtest/[0-999]\" for loadtesting.\n"))
+	})
+
+	for i := 0; i <= 999; i++ {
+		router.HandleFunc(fmt.Sprintf("/loadtest/%d", i), func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(time.Millisecond * 100)
+			w.Write([]byte("Greetings from \"" + r.URL.Path + "\"\n"))
+		})
+	}
+
+	http.Handle("/", router)
+
+	http.ListenAndServe(":8080", router)
+}
